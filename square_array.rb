747de94d2350b3944edb70511c751215c6e27f1b
Veronica Lopez <nicaa0695@gmail.com>
2019-07-17 16:59:32 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,7 @@
-def square_array(array)
-  array.each do |array|
-    puts array**
+def square_array(numbers)
+  new_numbers = []
+  numbers.each do |num|
+    new_numbers << (num ** 2)
+  end
+  return new_numbers
 end
